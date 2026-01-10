99b061599a18714c371528e5ac75f5dd455656a7
An Nguyen <josephus.an.nguyen@gmail.com>
2026-01-11 01:22:54 +0200

    Switched to full_address

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,7 +32,7 @@ const getSearchResults = () => {
         class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" />
       <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
         <li v-for="result in mapboxSearchResults" :key="result.id" class="p-2 cursor-pointer">
-          {{ result.properties.name }}
+          {{ result.properties.full_address }}
         </li>
       </ul>
     </div>
